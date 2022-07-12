7b0e943b8cd89e0e3b97ec494e26e4ddea513e23
Beatrice <b.paillusson@gmail.com>
2022-07-12 12:19:41 +0200

    commit0

---
+++ backend/routes/sauces.js
@@ -0,0 +1,18 @@
+// importation
+const express = require("express");
+const router = express.Router();
+
+const auth = require("../middlewares/auth");
+const multer = require("../middlewares/multer");
+
+const ctrlSauces = require("../controllers/sauces");
+
+// Routes
+router.get("/", auth, ctrlSauces.getAllSauces);
+router.post("/", auth, multer, ctrlSauces.createSauce);
+router.get("/:id", auth, ctrlSauces.getOneSauce);
+router.put("/:id", auth, multer, ctrlSauces.modifySauce);
+router.delete("/:id", auth, ctrlSauces.deleteSauce);
+router.post("/:id/like", auth, ctrlSauces.likeSauce);
+
+module.exports = router;
