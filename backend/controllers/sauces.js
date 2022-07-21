5fa0b840c7de77808acbf364debc3fd2b982b699
B.Paillusson <b.paillusson@gmail.com>
2022-07-21 11:22:24 +0200

    commit5

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -42,9 +42,9 @@ exports.modifySauce = (req, res, next) => {
   Sauce.findOne({_id: req.params.id})
   .then(sauce => {
     if (sauce.userId === req.auth.userId) {
-      console.log(sauce.userId);
-      console.log("auth");
-      console.log(req.auth.userId);
+      // console.log(sauce.userId);
+      // console.log("auth");
+      // console.log(req.auth.userId);
       res.status(403).json({error: 'Requête non authorisée'});
     } else {
       Sauce.updateOne(
